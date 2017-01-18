f90b119f6421adcd0ebb7c70e5c4ef9907fe0e30
MiGatoSeneca <pablo@citious.com>
2017-01-18 13:05:09 +0100

    install templates

--- app.js
+++ app.js
@@ -1,19 +1,26 @@
-var appconfig = require('./config/app.conf');
+const conf = require('./config/app.conf');
 
 var express = require('express');
 var bodyParser = require('body-parser');
+const pug = require('pug');
 
 var app = express();
+
 var server = require('http').createServer(app);
 
 var getUrls = require('get-urls');
 
 var scraper = require('./modules/insta-scraper');
 
+app.set('views',express.static('views'));
+app.set('view engine', 'jade');
+app.use('/public', express.static('public'));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
-server.listen(appconfig.port, function (err) {
+server.listen(conf.port, function (err) {
   if(!err){
-    console.log("Running at: "+appconfig.port);
+    console.log("Running at: "+conf.port);
   }
 });
 
@@ -24,93 +31,60 @@ app.use(function(req, res, next) {
   next();
 });
 
-app.use('/public', express.static('public'));
-
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-
 app.get('/api/version',function(req,res){
   var response={
-    "version":appconfig.version
+    "version":conf.version
   };
   res.status(200).jsonp(response);
 });
 
-app.get('/',function(req,res){
-	var html = "";
-	html += "<!DOCTYPE html>";
-	html += "<html lang='en'>";
-	html += "<head>";
-	html += "  <meta charset='utf-8'>";
-	html += "  <meta name='viewport' content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no'>";
-	html += "  <meta http-equiv='X-UA-Compatible' content='IE=edge'>";
-	html += "  <meta name='author' content='igLink'>";
-	html += "  <title>iglink - your Instagram link always updated</title>";
-	html += "  <meta name='description' content='With Iglink your profile link at Instagram will be always updated linking the right place'>";
-	html += "  <link href='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css' rel='stylesheet' integrity='sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u' crossorigin='anonymous'>";
-  html += "  <link rel='stylesheet' href='https://fonts.googleapis.com/css?family=Open+Sans:300,400' type='text/css'>";
-  html += "  <link rel='stylesheet' href='/public/css/app.css' type='text/css'>";
-  html += "  <script>";
-  html += "    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){";
-  html += "    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),";
-  html += "    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)";
-  html += "    })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');";
-  html += "  ";
-  html += "    ga('create', 'UA-90461818-1', 'auto');";
-  html += "    ga('send', 'pageview');";
-  html += "  ";
-  html += "  </script>";
-	html += "</head>";
-	html += "<body>";
-	html += "  <div class='container-fluid' id='main-container'/>";
-	html += "    <div class='row text-white p-y-150'>";
-	html += "      <div class='col-md-12 text-center'>";
-	html += "        <div class='text-center'>";
-	html += "          <img width='150px' src='/public/img/logo.png'/>";
-	html += "        </div>";
-	html += "        <h1>iglink.co</h1>";
-	html += "        <h2>Stop updating your Instagram link</h2>";
-	html += "        <h4>using iglink.co your biography link will be always updated</h4>";
-	html += "        <h4>with the link of your last post</h4>";
-	html += "        <div class='row p-t-20'>";
-	html += "          <div class='col-md-12 text-center height-150'>";
-	html += "            <form id='create-form'>";
-	html += "              <div class='row'>";
-	html += "                <div class='col-md-4 col-md-offset-4'>";
-	html += "                  <div class='form-group form-group-iglink'>";
-	html += "                    <input type='username' class='form-control' id='username' placeholder='Enter your instagram acount'>";
-	html += "                  </div>";
-	html += "                  <input type='submit' class='btn btn-primary btn-block' value='Create your link'/>";
-	html += "                </div>";
-  html += "              </div>";
-	html += "            </form>";
-	html += "            <div id='result-form' class='hidden'>";
-	html += "              <p id='result-iglink'>http://iglink.co/<span id='username'></span></p>";
-	html += "              <div class='row'>";
-	html += "                <div class='col-md-4 col-md-offset-4'>";
-	html += "                  <a id='result-copy-button' data-clipboard-text='' href='javascript:void(0);' class='btn btn-success btn-block'>Copy your link</a>";
-  html += "                  <div class='p-t-20'>";
-  html += "                     <a id='reset-form' href='javascript:void(0)' class='text-white'/>Create another link</a>";
-  html += "                  </div>";
-	html += "                </div>";
-	html += "              </div>";
-	html += "            </div>";
-	html += "          </div>";
-	html += "        </div>";
-	html += "      </div>";
-	html += "    </div>";
-	html += "  </div>";
-	html += "  <footer>";
-	html += "    <p class='text-center p-t-20'>created with lots of <span class='emoji'>❤️</span> by the <b>iglink team</b> © 2017</p>";
-	html += "  </footer>";
-	html += "  <script src='https://code.jquery.com/jquery-3.1.1.min.js' integrity='sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8=' crossorigin='anonymous'></script>";
-	html += "  <script src='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js' integrity='sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa' crossorigin='anonymous'></script>";
-  html += "  <script src='https://cdn.jsdelivr.net/clipboard.js/1.5.16/clipboard.min.js'></script>";
-  html += "  <script src='/public/js/app.js'></script>";
-	html += "</body>";
-	html += "</html>";
-	res.send(html);
+app.get('/', function(req, res, next) {
+  var data = conf.pug;
+  var html = pug.renderFile('./views/index.pug',data);
+  res.send(html);
 });
+
+app.get('/:username',function(req,res){
+  //Check if username Exists & is in the rigth format
+  scraper.getAccountInfo(req.params.username, function(error,data){
+    var html = "";
+    var url_found=false;
+    if(!error){
+      var url = "";
+      if((typeof data != "undefined") && (typeof data.media != "undefined") && (typeof data.media.nodes != "undefined")){
+        for ( var post of data.media.nodes){
+          if(post.caption != "undefined"){
+            if (url==""){
+              url = getUrls(post.caption);
+            }
+          }
+        }
+      }
+      if(url!=""){
+        url_found=true;
+      }
+    }
+    var data = conf.pug;
+    data.username = req.params.username;
+    if(url_found){
+      data.url = url;
+      var html = pug.renderFile('./views/redirection/index.pug',data);
+      res.send(html);
+    }else{
+      var html = pug.renderFile('./views/redirection/error/index.pug',data);
+      res.send(html);
+    }
+  });
+});
+
+app.get('/error',function(req,res){
+  var data = conf.pug;
+  var html = pug.renderFile('./views/error/index.pug',data);
+  res.send(html);
+});
+
+
+/*
 
 app.get('/:username',function(req,res){
 
@@ -133,7 +107,6 @@ app.get('/:username',function(req,res){
       }
     }
 
-
     html += "<!DOCTYPE html>";
     html += "<html lang='en'>";
     html += "<head>";
@@ -144,9 +117,11 @@ app.get('/:username',function(req,res){
     html += "  <meta name='author' content='igLink'>";
     html += "  <link href='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css' rel='stylesheet' integrity='sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u' crossorigin='anonymous'>";
     html += "  <link rel='stylesheet' href='https://fonts.googleapis.com/css?family=Open+Sans:300,400' type='text/css'>";
-    html += "  <link rel='stylesheet' href='./public/css/app.css' type='text/css'>";
+    link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400">
+    html += "  <link rel='stylesheet' href='./public/assets/css/app.css' type='text/css'>";
     html += "  <title>iglink - Redirection "+req.params.username+"</title>";
     html += "  <script>";
+
     html += "    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){";
     html += "    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),";
     html += "    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)";
@@ -162,10 +137,11 @@ app.get('/:username',function(req,res){
     html += "    <div class='row text-white p-y-150'>";
     html += "  	  <div class='col-md-12 text-center'>";
     html += "  	    <div class='text-center'>";
-    html += "  	      <img width='150px' src='./public/img/logo.png'/>";
+    html += "  	      <img width='150px' src='./public/assets/img/logo.png'/>";
     html += "  	    </div>";
     html += "  	    <h1>iglink.co</h1>";
     if(!url_found){
+
       html += "        <h2 class='p-t-20'>:(</h2>";
       html += "        <h4 class='p-t-20'>Sorry, there is no link to redirect you..</h4>";
       html += "        <div class='row p-t-20'>";
@@ -179,6 +155,9 @@ app.get('/:username',function(req,res){
       html += "          ga('send', 'event', 'redirection', 'redirection_error', '"+req.params.username+"');";
       html += "        </script>";
     }else{
+
+
+
       html += "        <h4 class='p-t-20'>Creating redirection..</h4>";
       html += "        <div class='spinner'>";
       html += "          <svg width='40px' height='40px' viewBox='0 0 66 66' xmlns='http://www.w3.org/2000/svg'>";
@@ -203,9 +182,12 @@ app.get('/:username',function(req,res){
       html += "  </script>";
     }
     html += "</body>";
+
     html += "</html>";
     html += "";
 
     res.send(html);
   });
 });
+
+*/
